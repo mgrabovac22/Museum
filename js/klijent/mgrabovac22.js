e1e09d529d542e8b56f7ef2d6428e0c90c0e05a6
Marin Grabovac <grabovacmarin000@gmail.com>
2024-05-20 21:56:31 +0200

    #15 Fixed issue with generating cookie

--- js/klijent/mgrabovac22.js
+++ js/klijent/mgrabovac22.js
@@ -9,28 +9,48 @@ document.addEventListener("DOMContentLoaded", function() {
         document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
     }
 
-    function mobile() {
-        if (body.classList.contains('mobile')) {
-            body.classList.remove('mobile');
-            toggleButton.textContent = "Prebaci na mobilnu verziju";
-            document.getElementById("navPocetna").style.display= "inline";
-            document.getElementById("kartica").style.display="inline";
-            document.getElementById("karticaMob").style.display="none";
-            document.getElementById("btnMobilni").style.display= "none";
-            setCookie("deviceVersion", "desktop", 7);
-        } else {
+    function getCookie(cname) {
+        const decodedCookie = decodeURIComponent(document.cookie);
+        const cookieArray = decodedCookie.split(';');
+        for (let i = 0; i < cookieArray.length; i++) {
+            let cookie = cookieArray[i].trim();
+            if (cookie.startsWith(cname + '=')) {
+                return cookie.substring(cname.length + 1); 
+            }
+        }
+        return null;
+    }
+    
+    function promjenaStanja() {
+        const deviceVersion = getCookie("deviceVersion");
+        if (deviceVersion === "mobile") {
             body.classList.add('mobile');
             toggleButton.textContent = "Vrati na stolnu verziju";
             document.getElementById("navPocetna").style.display= "none";
             document.getElementById("btnMobilni").style.display= "block";
             document.getElementById("kartica").style.display="none";
             document.getElementById("karticaMob").style.display="inline";
+        } else {
+            body.classList.remove('mobile');
+            toggleButton.textContent = "Prebaci na mobilnu verziju";
+            document.getElementById("navPocetna").style.display= "inline";
+            document.getElementById("kartica").style.display="inline";
+            document.getElementById("karticaMob").style.display="none";
+            document.getElementById("btnMobilni").style.display= "none";
+        }
+    }
+
+    function mobile() {
+        const deviceVersion = getCookie("deviceVersion");
+        if (deviceVersion === "mobile") {
+            setCookie("deviceVersion", "desktop", 7);
+        } else {
             setCookie("deviceVersion", "mobile", 7);
         }
+        promjenaStanja();
         console.log("Prebaceno je");
     }
+
     toggleButton.addEventListener('click', mobile);
+    promjenaStanja();
 });
-
-
-
