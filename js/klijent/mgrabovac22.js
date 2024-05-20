5613b688ddfc8750811fd8a4a31880b8d62c68c9
Marin Grabovac <grabovacmarin000@gmail.com>
2024-05-20 22:19:00 +0200

    #15 Some renaming and restructuring

--- js/klijent/mgrabovac22.js
+++ js/klijent/mgrabovac22.js
@@ -1,56 +1,57 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const toggleButton = document.getElementById('mobile');
+    const dugme = document.getElementById('mobile');
     const body = document.body;
 
-    function setCookie(cname, cvalue, exdays) {
+    function setCookie(nazivKolacica, vrijednostKolacica, rokTrajanja) {
         const d = new Date();
-        d.setTime(d.getTime() + (exdays*24*60*60*1000));
-        let expires = "expires=" + d.toUTCString();
-        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+        d.setTime(d.getTime() + (rokTrajanja*24*60*60*1000));
+        let istjece = "expires=" + d.toUTCString();
+        document.cookie = nazivKolacica + "=" + vrijednostKolacica + ";" + istjece + ";path=/";
     }
 
-    function getCookie(cname) {
-        const decodedCookie = decodeURIComponent(document.cookie);
-        const cookieArray = decodedCookie.split(';');
-        for (let i = 0; i < cookieArray.length; i++) {
-            let cookie = cookieArray[i].trim();
-            if (cookie.startsWith(cname + '=')) {
-                return cookie.substring(cname.length + 1); 
-            }
-        }
-        return null;
-    }
     
     function promjenaStanja() {
-        const deviceVersion = getCookie("deviceVersion");
-        if (deviceVersion === "mobile") {
+        const verzija = uzmiKolacic("verzija");
+        if (verzija == "mobile") {
             body.classList.add('mobile');
-            toggleButton.textContent = "Vrati na stolnu verziju";
+            dugme.textContent = "Vrati na stolnu verziju";
             document.getElementById("navPocetna").style.display= "none";
             document.getElementById("btnMobilni").style.display= "block";
             document.getElementById("kartica").style.display="none";
             document.getElementById("karticaMob").style.display="inline";
         } else {
             body.classList.remove('mobile');
-            toggleButton.textContent = "Prebaci na mobilnu verziju";
+            dugme.textContent = "Prebaci na mobilnu verziju";
             document.getElementById("navPocetna").style.display= "inline";
             document.getElementById("kartica").style.display="inline";
             document.getElementById("karticaMob").style.display="none";
             document.getElementById("btnMobilni").style.display= "none";
         }
     }
-
-    function mobile() {
-        const deviceVersion = getCookie("deviceVersion");
-        if (deviceVersion === "mobile") {
-            setCookie("deviceVersion", "desktop", 7);
-        } else {
-            setCookie("deviceVersion", "mobile", 7);
+    
+    function uzmiKolacic(nazivKolacica) {
+        const decodedCookie = decodeURIComponent(document.cookie);
+        const vrijednostKolacica = decodedCookie.split(';');
+        for (let i = 0; i < vrijednostKolacica.length; i++) {
+            let cookie = vrijednostKolacica[i].trim();
+            if (cookie.startsWith(nazivKolacica + '=')) {
+                return cookie.substring(nazivKolacica.length + 1); 
+            }
+        }
+        return null;
+    }
+    
+    function prebaci() {
+        const verzija = uzmiKolacic("verzija");
+        if (verzija == "mobile") {
+            setCookie("verzija", "desktop", 7);
+        } 
+        else {
+            setCookie("verzija", "mobile", 7);
         }
         promjenaStanja();
-        console.log("Prebaceno je");
     }
 
-    toggleButton.addEventListener('click', mobile);
+    dugme.addEventListener('click', prebaci);
     promjenaStanja();
 });
