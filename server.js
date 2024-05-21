9e017e78066338778044b68306eb8ad929889170
Marin Grabovac <grabovacmarin000@gmail.com>
2024-05-21 12:05:50 +0200

    #17 Served some pages and css and resurses folder

--- server.js
+++ server.js
@@ -5,10 +5,22 @@ const server = express();
 const port = 12073;
 
 let fun = function(request, response) {
-    response.send('Hello world');
+    response.sendFile(__dirname + "/html/index.html");
 };
-  
+let funCSS = function(request, response) {
+    response.sendFile(__dirname + "/css");
+};
+let funJS = function(request, response) {
+    response.sendFile(__dirname + "/jsk");
+};
+let funSlike = function(request, response) {
+    response.sendFile(__dirname + "/slike");
+};
+
 server.get('/', fun);
+server.use("/css", express.static("./css"));
+server.use("/jsk", express.static("./js/klijent"));
+server.use("/slike", express.static("./resursi/slike"));
 
 server.listen(port, () => {
     http = "http://localhost:";
