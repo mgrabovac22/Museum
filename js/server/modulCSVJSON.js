64393b4ac7307576cbef4e0c97826bb0cf4ef30d
Marin Grabovac <grabovacmarin000@gmail.com>
2024-05-22 13:47:13 +0200

    #17 Made functions that handle tranforms from json to csv and back

---
+++ js/server/modulCSVJSON.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const fs = require('fs');
+
+class pomFunkJSONCSV {
+    constructor() {
+        console.log("Klasa inicijalizirana");
+    }
+
+    kopirajJSONuCSV() {
+        let putanjaJSON = path.join(__dirname, 'izlozba.json');
+        let putanjaCSV = path.join(__dirname, 'izlozba.csv');
+
+        fs.readFile(putanjaJSON, 'utf8', (err, jsonPodaci) => {
+            if (err) console.log(err);
+    
+            const primjerci = JSON.parse(jsonPodaci);
+            const csvpodaci = primjerci.map(primjerak => Object.values(primjerak).join('#')).join('\n');
+    
+            fs.writeFile(putanjaCSV, csvpodaci, 'utf8', (err) => {
+                if (err) console.log(err);
+            });
+        });
+    }
+
+    prebaciCSVuJSON(csvLinija) {
+        const nazivVarijabli = ['id', 'naziv', 'opis', 'kategorija'];
+        const vrijednosti = csvLinija.split('#');
+        const rezultat = {};
+        nazivVarijabli.forEach((nazivVarijable, index) => {
+            rezultat[nazivVarijable] = vrijednosti[index];
+        });
+        return rezultat;
+    }
+
+    prebaciJSONuCSV(jsonObject) {
+        const vrijednosti = ['id', 'naziv', 'opis', 'kategorija'].map(nazivVarijable => jsonObject[nazivVarijable]);
+        return vrijednosti.join('#');
+    }
+};
+
+module.exports = pomFunkJSONCSV;
